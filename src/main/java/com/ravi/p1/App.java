fa43ea6276c5ce63b78163a38cd50ccb836a89d3
ravi2krishna <ravi2krishna@gmail.com>
2019-12-30 11:43:31 +0000

    code

--- src/main/java/com/ravi/p1/App.java
+++ src/main/java/com/ravi/p1/App.java
@@ -9,5 +9,6 @@ package com.ravi.p1;
     public static void main( String[] args )
     {
         System.out.println( "Working with Jenkins" );
+        System.out.println( "New Change Test" );
     }
 }
