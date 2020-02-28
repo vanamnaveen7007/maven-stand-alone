c29d6381399f403a6970ca3230dc3d7716f4ae7e
ravi <ravi2krishna@gmail.com>
2020-02-28 12:54:01 +0530

    print msg

--- src/main/java/com/ravi/p1/App.java
+++ src/main/java/com/ravi/p1/App.java
@@ -1,19 +1,11 @@
 package com.ravi.p1;
-import org.apache.log4j.Logger;
 
-/**
- * Hello world!
- *
- */
- public class App  
+public class App  
 {
    
-    // Implemenet Log4j for debug
-    static Logger log = Logger.getLogger(App.class.getName());
 
     public static void main( String[] args )
     {
-        //System.out.println( "COD implemented" );
-	log.debug("COD implemented");
+        System.out.println( "COD implemented" );
     }
 }
