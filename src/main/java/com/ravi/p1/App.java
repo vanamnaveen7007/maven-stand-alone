973a26545a01a4495ac9c6964ff10515c6f19c6b
Ravi <ravi2krishna@gmail.com>
2020-03-02 12:58:45 +0530

    Update App.java

--- src/main/java/com/ravi/p1/App.java
+++ src/main/java/com/ravi/p1/App.java
@@ -5,6 +5,6 @@ public class App
    
     public static void main( String[] args )
     {
-        System.out.println( "COD implemented" );
+        System.out.println( "Master Slave Test On Node1" );
     }
 }
