197361873fff66c8829122a94c3d576bfce7f9e0
ravi2krishna <ravi2krishna@gmail.com>
2020-03-26 17:25:34 +0530

    ws

--- src/main/java/com/ravi/p1/App.java
+++ src/main/java/com/ravi/p1/App.java
@@ -5,6 +5,6 @@ public class App
    
     public static void main( String[] args )
     {
-        System.out.println( "Master Slave Test On Node1" );
+        System.out.println( "Check from Local Workstation" );
     }
 }
